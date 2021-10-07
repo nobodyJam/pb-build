ac67f3e6acb571ffb99055378674adb763af78d5
nobodyJam <1257400056@qq.com>
2021-10-08 00:07:37 +0800

    Update testgit.bat

--- testgit.bat
+++ testgit.bat
@@ -1,5 +1,6 @@
 echo 开始测试
 echo %USERNAME%
 start /WAIT git config --global credential.helper store
->C:\Users\%USERNAME%\.git-credentials set /p="https://18390894512:5201107jjw.@e.coding.net"<nul
-git clone https://e.coding.net/ybzncoding/tongyongjiekouchengxu/tyjk.git
+git config -l
+REM >C:\Users\%USERNAME%\.git-credentials set /p="https://18390894512:5201107jjw.@e.coding.net"<nul
+REM git clone https://e.coding.net/ybzncoding/tongyongjiekouchengxu/tyjk.git
